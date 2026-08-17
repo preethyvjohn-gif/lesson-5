f8bfac4a99d7292a23c5afe395996977ffc439a5
preethyvjohn-gif <preethyvjohn@gmail.com>
2026-08-17 18:45:47 +0930

    commit

--- project.py
+++ project.py
@@ -26,6 +26,18 @@ if weather == "sunny" and homework == "yes":
 
 if weather == "rainy" or weather == "cloudy":
     print("Weather tip : pack your Umbrella - it may get wet outside.")
+
+if not (homework == "yes"):
+    print("Homework     : Not done yet. Finish it before going out!")
+
+if weather == "rainy" and not (homework == "yes"):
+    print("Best plan    : Stay in, finnish homework, and then watch your favourite show.")
+elif day in("Saturday", "Sunday") and weather == "sunny":
+    print("Best plan   : perfect weekend weather - head outside and have some fun!")
+else:
+    print("Best plan   : Take it one step at a time - you have got this!")
+print()
+print("plan complete! have a wonderful day")
     
 
 
